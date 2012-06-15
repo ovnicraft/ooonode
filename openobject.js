07b37aaa0273fc225701ee17e0ebae94282d8206
Cristian Salamea <ovnicraft@gmail.com>
2012-06-15 11:21:23 -0500

    [FIX] instance proxy

--- openobject.js
+++ openobject.js
@@ -3,21 +3,27 @@
  */
 var xmlrpc = require('xmlrpc');
 
-var ProxyRpc = function(dbname, host, user, passwd) {
-    this.database = dbname;
-    this.host = host;
-    this.user = user;
-    this.password = passwd;
-    this.proxyOptions = {
-      host: this.host,
-      port: this.port,
-      path:'/xmlrpc/common',    
-    };
-    this.client = xmlrpc.createClient(this.proxyOptions);
+var openobject = exports
+
+ProxyRpc = function(dbname, host, port, user, passwd) {
+  this.database = dbname;
+  this.host = host;
+  this.port = port;
+  this.user = user;
+  this.password = passwd;
+  this.proxyOptions = {
+    host: this.host,
+    port: this.port,
+    path:'/xmlrpc/common'
+  };
+  this.client = xmlrpc.createClient(this.proxyOptions);
+  console.log('Proxy instance creating...');
+  console.log('Creating rpc client...');
 };
 
 ProxyRpc.prototype = {
   login: function (arguments){
+    console.log('Login database: '+this.database);
     this.client.methodCall('login', [this.database, this.user, this.password], function(error, user_id){
 			console.log(user_id);
 			return user_id;
